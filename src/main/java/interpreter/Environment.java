b5c4a5a8c9ffa4a0b6d28af754361811efec93ac
Erik Edrosa <erik.edrosa@gmail.com>
2015-09-24 18:41:49 -0400

    Fix Environment brace style and add lookUp

--- src/main/java/interpreter/Environment.java
+++ src/main/java/interpreter/Environment.java
@@ -2,33 +2,37 @@ package interpreter;
 
 import java.util.Hashtable;
 
-public class Environment
-{
+public class Environment {
     private Environment parent;
     private Hashtable<String, SchemeObject> bindings;
 
-    public Environment()
-    {
+    public Environment() {
         this(null, null, null);
     }
 
-    public Environment(Environment parent, SchemePair params, SchemePair args)
-    {
+    public Environment(Environment parent, SchemePair params, SchemePair args) {
         this.parent = parent;
         this.bindings = new Hashtable<String, SchemeObject>();
     }
 
-    public void define(String name, SchemeObject value)
-    {
+    public void define(String name, SchemeObject value) {
         this.bindings.put(name, value);
     }
 
-    public void set(String name, SchemeObject value)
-    {
+    public void set(String name, SchemeObject value) {
         if(!this.bindings.containsKey(name)) {
             // error, binding not found
         } else {
             this.bindings.put(name, value);
         }
+    }
+
+    public SchemeObject lookUp(String name) {
+        SchemeObject obj = this.bindings.get(name);
+        if(obj == null && parent != null)
+            obj = parent.lookUp(name);
+
+        // TODO: check if variable still not found
+        return obj;
     }
 }
