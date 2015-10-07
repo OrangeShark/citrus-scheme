f92a4096be22abfc6dbdb928caa9914e54688238
Erik Edrosa <erik.edrosa@gmail.com>
2015-10-06 22:07:13 -0400

    Add - * / to scheme base

--- src/main/java/interpreter/scheme/Base.java
+++ src/main/java/interpreter/scheme/Base.java
@@ -24,13 +24,76 @@ public class Base extends Library {
 
     public class Minus extends Primitive {
         public SchemeObject apply(Interpreter interpreter, SchemePair args) {
-            return null;
+            if(args == null) {
+                throw new IllegalArgumentException("not enough arguments");
+            }
+            if(!(args.head instanceof SchemeNumber)) {
+                throw new IllegalArgumentException("argument is not a Number");
+            }
+            double total = ((SchemeNumber)args.head).value;
+            if(args.tail == null) {
+                return new SchemeNumber(-total);
+            }
+            args = SchemePair.of(args.tail);
+            while(args != null) {
+                SchemeObject element = args.head;
+                if(!(element instanceof SchemeNumber)) {
+                    throw new IllegalArgumentException("argument is not a Number");
+                }
+                total -= ((SchemeNumber)element).value;
+                args = SchemePair.of(args.tail);
+            }
+            return new SchemeNumber(total);
+        }
+    }
+
+    public class Times extends Primitive {
+        public SchemeObject apply(Interpreter interpreter, SchemePair args) {
+            double total = 1.0;
+            while(args != null) {
+                SchemeObject element = args.head;
+                if(element instanceof SchemeNumber) {
+                    total *= ((SchemeNumber)element).value;
+                } else {
+                    throw new IllegalArgumentException("argument is not a Number");
+                }
+                args = SchemePair.of(args.tail);
+            }
+            return new SchemeNumber(total);
+        }
+    }
+
+    public class Divide extends Primitive {
+        public SchemeObject apply(Interpreter interpreter, SchemePair args) {
+            if(args == null) {
+                throw new IllegalArgumentException("not enough arguments");
+            }
+            if(!(args.head instanceof SchemeNumber)) {
+                throw new IllegalArgumentException("argument is not a Number");
+            }
+            double total = ((SchemeNumber)args.head).value;
+            if(args.tail == null) {
+                return new SchemeNumber(1 / total);
+            }
+            args = SchemePair.of(args.tail);
+            while(args != null) {
+                SchemeObject element = args.head;
+                if(!(element instanceof SchemeNumber)) {
+                    throw new IllegalArgumentException("argument is not a Number");
+                }
+                total /= ((SchemeNumber)element).value;
+                args = SchemePair.of(args.tail);
+            }
+            return new SchemeNumber(total);
         }
     }
 
 
     public void importLib(Environment env) {
         env.define("+", new Plus());
+        env.define("-", new Minus());
+        env.define("*", new Times());
+        env.define("/", new Divide());
 
         // Special forms
         env.define("if", new Syntax(Syntax.Special.IF));
